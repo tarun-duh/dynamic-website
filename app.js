71ab31b5bee05d39c0adcd43064d42102d914f0e
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2023-02-17 23:22:39 +0530

    .

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 function animateSlides() {
   controllers = new ScrollMagic.Controller();
   const sliders = document.querySelectorAll(".slide");
-  const nav = documnet.querySelector(".nav-header");
+  const nav = document.querySelector(".nav-header");
   sliders.forEach((slide) => {
     const revealImg = slide.querySelectorAll(".reveal-img");
-    const img = slide.querySelectorAll("'hero-img");
+    const img = slide.querySelectorAll(".hero-img");
     const reavealText = slide.querySelectorAll(".reveal-text");
+    // gsap.to(revealImg, 1, { x: "100%" });
+    // gsap.to(reavealText, 1, { x: "100%" });
     const slideT1 = gsap.timeline({
       defaults: { duration: 1, ease: "power2.inOut" },
-    }).fromTo;
+    });
+    slideT1.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideT1.fromTo(img, { scale: "2" }, { scale: "1" });
+    slideT1.fromTo(reavealText, { x: "0%" }, { x: "100%" });
   });
 }
+animateSlides();
